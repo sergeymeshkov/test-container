9c2a929db94683d8693e22d578ceea3517073604
siarhei.miashkou <siarhei.miashkou@sap.com>
2023-02-13 18:53:08 +0300

    Remove spring boot testing

--- src/test/java/com/example/testcontainer/TestContainerApplicationTests.java
+++ src/test/java/com/example/testcontainer/TestContainerApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.testcontainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TestContainerApplicationTests {
 
 	@Test
